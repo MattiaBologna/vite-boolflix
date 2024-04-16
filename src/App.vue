8a5807dcc3ed1c9a261900751ef4a317ce3d7afa
MattiaBologna <mattiabologna01@gmail.com>
2024-04-16 12:17:32 +0200

    Add

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import { store } from './store.js';
+
+export default {
+
+}
+</script>
+
+<template>
+  <h1>Ciaone</h1>
+</template>
+
+<style lang="scss">
+@use './components/style/general';
+</style>
