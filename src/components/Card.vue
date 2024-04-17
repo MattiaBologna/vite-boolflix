64cac12a7c2742b397f950417eeccad77ce91719
MattiaBologna <mattiabologna01@gmail.com>
2024-04-17 15:45:31 +0200

    Add: flags

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -16,13 +16,41 @@ export default {
     <li>
         <p>{{ item.title }}</p>
         <p>{{ item.original_title }}</p>
-        <p>{{ item.original_language }}</p>
-        <p>{{ item.vote_average }}</p>
-        <font-awesome-icon :icon="['fas', 'star']" />
+        <div class="language">
+            <p>{{ item.original_language }}</p>
+            <img v-if="item.original_language === 'en' || item.original_language === 'it' || item.original_language === 'es'"
+                :src="'/public/' + item.original_language + '_flag.svg'" alt="usa flag" class="flag">
+            <img v-else src="/public/earth_flag.svg" alt="earth flag" class="flag">
+
+        </div>
+        <div class="vote_average">
+            <font-awesome-icon v-for="n in Math.floor(item.vote_average / 2)" :icon="['fas', 'star']" />
+            <font-awesome-icon v-for="n in (5 - Math.floor(item.vote_average / 2))" :icon="['far', 'star']" />
+            <p class="vote_average_number">{{ item.vote_average }}</p>
+        </div>
+
     </li>
 
 </template>
 
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.vote_average {
+    display: flex;
+
+    .vote_average_number {
+        margin-left: 15px;
+    }
+}
+
+.language {
+
+    display: flex;
+    gap: 10px;
+
+    .flag {
+        max-width: 30px;
+    }
+}
+</style>
